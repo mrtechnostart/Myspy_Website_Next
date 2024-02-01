1cf220003c7bc02129f94021fb4912248ca6c440
mrtechnostart <rambpandey238@gmail.com>
2024-02-02 00:04:09 +0530

    Done With CRUD

--- src/app/api/project/route.js
+++ src/app/api/project/route.js
@@ -23,18 +23,54 @@ export async function POST(req) {
 export async function GET(req) {
   try {
     const token = await getToken({ req: req });
-    if (true) {
-      const response = await prisma.persona.findMany({
-        select: {
-          persona: true,
-          id: true,
-        },
-      });
+    if (token && token.user.id) {
+      const response = await prisma.project.findMany({});
+      console.log(response);
       return NextResponse.json({ data: response }, { status: 200 });
     }
     return NextResponse.json({ data: "unauthorized" }, { status: 500 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ status: 500 }, { error: error });
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    const token = await getToken({ req: req });
+    const data = await req.json();
+    const { id, ...restData } = data;
+    if (token && token.user.id) {
+      const response = await prisma.project.update({
+        where: {
+          id: id,
+        },
+        data: restData,
+      });
+      return NextResponse.json({ data: response }, { status: 200 });
+    }
+    return NextResponse.json({ data: "unauthorized" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
+export async function DELETE(request) {
+  const token = await getToken({ req: request });
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  try {
+    if (token && token.user.id) {
+      const data = await prisma.project.delete({
+        where: {
+          id: id.toString(),
+        },
+      });
+      return NextResponse.json({ data: data }, { status: 200 });
+    }
+    return NextResponse.json({ data: "unauthorized" }, { status: 500 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
--- src/app/myComponents/ViewProjects.jsx
+++ src/app/myComponents/ViewProjects.jsx
@@ -1,12 +1,6 @@
 "use client";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Table,
   TableBody,
@@ -15,53 +9,144 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { IoIosArrowDropdownCircle } from "react-icons/io";
+import AlertDialogCustom from "./AlertDialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useDebounce } from "@/lib/useDebounce";
+import { Textarea } from "@/components/ui/textarea";
 
-function DropDown() {
+function DialogDemo({ data, updater }) {
+  const [formData, setFormData] = useState(data);
+  const [isLoading, setLoading] = useState(false);
+  const { toast } = useToast();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.patch("/api/project", formData);
+      updater((prev) => {
+        return prev.map((element) => {
+          if (element.id === data.id) {
+            return { ...element, projectName: formData.projectName };
+          }
+          return element;
+        });
+      });
+      if (response.status === 200)
+        toast({
+          description: "Successfull ✅",
+        });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+    console.log(formData);
+  }
   return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <IoIosArrowDropdownCircle size={30} />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Perform Actions</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Edit</DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-3">
+            <Input
+              type="text"
+              name="persona"
+              id="persona"
+              value={formData.projectName}
+              onChange={(e) =>
+                setFormData({ ...formData, projectName: e.target.value })
+              }
+              placeholder="Project Name: "
+            />
+            <Input
+              type="text"
+              name="minDesc"
+              id="minDesc"
+              value={formData.minDesc}
+              onChange={(e) =>
+                setFormData({ ...formData, minDesc: e.target.value })
+              }
+              placeholder="Short Header: "
+            />
+            <Textarea
+              placeholder="Project Short Description"
+              name="desc"
+              id="desc"
+              value={formData.desc}
+              onChange={(e) =>
+                setFormData({ ...formData, desc: e.target.value })
+              }
+            />
+            <Input
+              type="text"
+              name="hrefto"
+              id="hrefto"
+              value={formData.hrefto}
+              onChange={(e) =>
+                setFormData({ ...formData, hrefto: e.target.value })
+              }
+              placeholder="Link of Project: "
+            />
+            <DialogFooter>
+              <Button
+                type="submit"
+                className="my-3"
+                variant="outline"
+                disabled={isLoading}
+              >
+                Update Now
+              </Button>
+            </DialogFooter>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
 export default function ViewProjects() {
-  const PersonaData = [
-    {
-      Persona: "User 1",
-      ProjectName: "Project A",
-      Description: "Lorem ipsum dolor sit amet",
-    },
-    {
-      Persona: "User 2",
-      ProjectName: "Project B",
-      Description: "Consectetur adipiscing elit",
-    },
-    {
-      Persona: "User 3",
-      ProjectName: "Project C",
-      Description: "Sed do eiusmod tempor incididunt",
-    },
-    {
-      Persona: "User 4",
-      ProjectName: "Project D",
-      Description: "Ut labore et dolore magna aliqua",
-    },
-    {
-      Persona: "User 5",
-      ProjectName: "Project E",
-      Description: "Duis aute irure dolor in reprehenderit",
-    },
-  ];
+  const [ProjectData, setProjectData] = useState([
+    { projectName: "Fetching", id: "1" },
+  ]);
+
+  async function handleDelete(id) {
+    const newData = ProjectData.filter((element) => element.id !== id);
+    setProjectData(newData);
+    try {
+      const response = await axios.delete(`/api/project?id=${id}`);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("/api/project");
+      console.log(response.data.data);
+      setProjectData(response.data.data);
+    })();
+  }, []);
+
   return (
     <>
       <div className="w-2/3 mx-auto flex justify-center">
@@ -69,22 +154,24 @@ export default function ViewProjects() {
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">S.No</TableHead>
-              <TableHead>Persona</TableHead>
               <TableHead>Project Name</TableHead>
-              <TableHead>Description</TableHead>
               <TableHead className="text-right">Options</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {PersonaData.map((element, index) => {
+            {ProjectData.map((element, index) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={element.id}>
                   <TableCell className="font-medium">{index + 1}</TableCell>
-                  <TableCell>{element.Persona}</TableCell>
-                  <TableCell>{element.ProjectName}</TableCell>
-                  <TableCell>{element.Description}</TableCell>
-                  <TableCell className="text-right">
-                    <DropDown />
+                  <TableCell>{element.projectName}</TableCell>
+                  <TableCell className=" flex justify-end space-x-3 items-center">
+                    <DialogDemo data={element} updater={setProjectData} />
+                    <AlertDialogCustom
+                      Name="Delete"
+                      variant="destructive"
+                      id={element.id}
+                      deletehandler={handleDelete}
+                    />
                   </TableCell>
                 </TableRow>
               );
