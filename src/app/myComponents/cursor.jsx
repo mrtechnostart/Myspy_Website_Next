d228b00423b3b29e145a7120edde86f4018f24f9
mrtechnostart <rambpandey238@gmail.com>
2024-02-16 10:14:26 +0530

    Show Conditional Mouse Pointer

--- src/app/myComponents/cursor.jsx
+++ src/app/myComponents/cursor.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import AnimatedCursor from "react-animated-cursor";
 
 export default function Cursor() {
-  return (
-    <div className="App">
-      <AnimatedCursor />
-    </div>
-  );
+  const [currentWidth, setCurrentWidth] = useState(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setCurrentWidth(window.outerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    setCurrentWidth(window.outerWidth);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  console.log(currentWidth);
+  return <>{currentWidth > 700 && <AnimatedCursor />}</>;
 }
