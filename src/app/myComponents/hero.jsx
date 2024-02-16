40223a350574450252a3ce5d85d2edbf0835c27c
mrtechnostart <rambpandey238@gmail.com>
2024-02-16 09:51:17 +0530

    Make Typewriter Inline

--- src/app/myComponents/hero.jsx
+++ src/app/myComponents/hero.jsx
@@ -5,22 +5,26 @@ export default function Hero() {
     <>
       <div className="flex items-left justify-left h-screen mx-20">
         <div>
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl flex-1">
-            Hey there, it{"'s"}
-            <Typewriter
-              options={{
-                strings: [
-                  "Myspy",
-                  "Madhusudhan",
-                  "Maneswar",
-                  "Captain X",
-                  "Nicof",
-                ],
-                autoStart: true,
-                loop: true,
-              }}
-            />
+          <div className="flex flex-row">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl flex-1">
+              Hey there, it{"'s"}
             </h1>
+            <h1 className="ml-3 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
+              <Typewriter
+                options={{
+                  strings: [
+                    "Myspy",
+                    "Madhusudhan",
+                    "Maneswar",
+                    "Captain X",
+                    "Nicof",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
+          </div>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             Divided by Personas, United by Purpose.
           </p>
