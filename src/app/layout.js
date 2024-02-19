1958de9aa9aa52d916cb6198aa79556bd52485f0
mrtechnostart <rambpandey238@gmail.com>
2024-02-19 17:19:08 +0530

    EnableSystem=False

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
-            enableSystem
+            enableSystem={false}
             disableTransitionOnChange
           >
             {children}
