65e5d15c16bfbb55d4d97c3f122bd4a94c250238
mrtechnostart <rambpandey238@gmail.com>
2024-02-19 17:00:18 +0530

    Default theme flag added and set to dark

--- src/app/myComponents/themeProvider.jsx
+++ src/app/myComponents/themeProvider.jsx
@@ -4,5 +4,9 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props} defaultTheme="dark">
+      {children}
+    </NextThemesProvider>
+  );
 }
