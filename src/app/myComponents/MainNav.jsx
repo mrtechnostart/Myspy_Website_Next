1b1205174f5e07a884f6bdd6de59bc130c5f77a4
mrtechnostart <rambpandey238@gmail.com>
2024-02-25 19:05:06 +0530

    NewTab Thing

--- src/app/myComponents/MainNav.jsx
+++ src/app/myComponents/MainNav.jsx
@@ -22,6 +22,7 @@ export function MainNav({ className, ...props }) {
         <>
           {" "}
           <Link
+            target="_blank"
             href="https://dhnz.short.gy/discussion"
             className="text-sm font-medium transition-colors hover:text-primary"
           >
